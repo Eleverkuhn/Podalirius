555faea5f6832ff35f59e5e00f0a500d0a4180ca
Eleverkhun <Eleverkuhn@users.noreply.github.com>
2025-09-04 11:07:42 +0300

    refactor(tests): Logic of tests of Redis connection was rewritten

--- tests/test_unit/data/test_redis_connection.py
+++ tests/test_unit/data/test_redis_connection.py
@@ -1,42 +1,33 @@
+import importlib
+
 import pytest
-from redis import Redis, AuthenticationError, ConnectionError
+from redis import AuthenticationError, ConnectionError
 
-from config import Config
-from data.redis import redis_conn
-
-settings = Config.get_settings()
+import config
+import data.redis_config as r_conf
 
 
 class TestRedisConn:
+    def reload_imports(self) -> None:
+        importlib.reload(config)
+        importlib.reload(r_conf)
+
+    def ping_redis_with_error(self, error: Exception) -> None:
+        self.reload_imports()
+        with pytest.raises(error):
+            r_conf.redis_conn.ping()
+
     def test_conn(self) -> None:
-        assert redis_conn.ping()
+        assert r_conf.redis_conn.ping()
 
-    def test_incorrect_host_raises_conn_err(self) -> None:
-        redis_conn = Redis(
-            host="wronghost",
-            password=settings.redis_password,
-            port=settings.redis_port,
-            decode_responses=True
-        )
-        with pytest.raises(ConnectionError):
-            redis_conn.ping()
+    def test_incorrect_host_raises_conn_err(self, monkeypatch) -> None:
+        monkeypatch.setenv("REDIS_HOST", "incorrect_host")
+        self.ping_redis_with_error(ConnectionError)
 
-    def test_incorrect_pass_raises_auth_err(self) -> None:
-        redis_conn = Redis(
-            host=settings.redis_host,
-            password="wrongpass",
-            port=settings.redis_port,
-            decode_responses=True
-        )
-        with pytest.raises(AuthenticationError):
-            redis_conn.ping()
+    def test_incorrect_pass_raises_auth_err(self, monkeypatch) -> None:
+        monkeypatch.setenv("REDIS_PASSWORD", "incorrect_password")
+        self.ping_redis_with_error(AuthenticationError)
 
-    def test_incorrect_port_raises_conn_err(self) -> None:
-        redis_conn = Redis(
-            host=settings.redis_host,
-            password=settings.redis_password,
-            port=6078,
-            decode_responses=True
-        )
-        with pytest.raises(ConnectionError):
-            redis_conn.ping()
+    def test_incorrect_port_raises_conn_err(self, monkeypatch) -> None:
+        monkeypatch.setenv("REDIS_PORT", "6666")
+        self.ping_redis_with_error(ConnectionError)
