d7da39a8bc4c16fcd3275b579f78e69cd47d9b75
Eleverkhun <Eleverkuhn@users.noreply.github.com>
2025-10-01 11:55:46 +0300

    refactor(crud): changed behavior of BaseCRUD class
    Now base class works only with SQLModel instances.
    Also changed logging DEBUG messages to an external file.

--- src/data/crud.py
+++ src/data/crud.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from pydantic import BaseModel
 from sqlmodel import SQLModel, Session, select
 
@@ -8,47 +10,72 @@ class BaseCRUD:
     def __init__(
             self,
             session: Session,
-            sql_model: BaseSQLModel | None = None,
-            return_model: BaseModel | None = None) -> None:
+            sql_model: type[BaseSQLModel]) -> None:
         self.session = session
         self.sql_model = sql_model
-        self.return_model = return_model  # TODO: probably need to remove this
 
-    def create_raw(self, model: BaseModel) -> SQLModel:
-        # TODO: remove this on the beneath one
+    @property
+    def select(self):
+        return select(self.sql_model)
+
+    def add(self, instance: BaseSQLModel) -> BaseSQLModel:
+        self.session.add(instance)
+        self.session.flush()
+        return instance
+
+    def get(self, id: int | bytes) -> BaseSQLModel:
+        statement = self.select.where(self.sql_model.id == id)
+        entry = self.session.exec(statement)
+        return entry.one()
+
+    def get_all(self):
+        return self.session.exec(self.select).all()
+
+    def update(self, entry: BaseSQLModel, data: dict) -> BaseSQLModel:
+        data["updated_at"] = datetime.now()
+        for key, value in data.items():
+            if hasattr(entry, key):
+                setattr(entry, key, value)
+        entry = self.add(entry)
+        self.session.commit()
+        return entry
+
+    def delete(self, entry: BaseSQLModel) -> None:
+        self.session.delete(entry)
+        self.session.commit()
+
+    def _construct_sql_model_from_dict(self, data: dict) -> BaseSQLModel:
+        return self.sql_model(**data)
+
+    def create_raw(self, model: BaseModel) -> SQLModel:  # FIX: deprecated
         entry = self.sql_model(**model.model_dump())
         return self._add(entry)
 
-    def create_raw_from_dict(self, data: dict) -> SQLModel:
+    def create_raw_from_dict(self, data: dict) -> SQLModel:  # FIX: deprecated
         # TODO: this method should be main creation method
         entry = self.sql_model(**data)
         return self._add(entry)
 
-    def get_raw(self, id: int | bytes) -> None:
+    def get_raw(self, id: int | bytes) -> None:  # FIX: deprecated
         with self.session as session:
             statement = select(self.sql_model).where(self.sql_model.id == id)
             result = session.exec(statement)  # TODO:: don't like the naming 
             return result.one()
 
-    def get_all(self) -> list[BaseSQLModel]:
-        statement = select(self.sql_model)
-        entries = self.session.exec(statement)
-        return entries.all()
-
-    def update_raw(self, id: str | int, data: dict) -> None:
+    def update_raw(self, id: str | int, data: dict) -> None:  # FIX: deprecated
         entry = self.get_raw(id)
         for key, value in data.items():
             if hasattr(entry, key):
                 setattr(entry, key, value)
         return self._add(entry)
 
-    def delete_raw(self, id: int | bytes) -> None:
+    def delete_raw(self, id: int | bytes) -> None:  # FIX: deprecated
         entry = self.get_raw(id)
         with self.session as session:
             session.delete(entry)
             session.commit()
 
-    def _add(self, entry) -> None:
+    def _add(self, entry) -> None:  # FIX: depreacated
         with self.session as session:
             session.add(entry)
             session.commit()
---
+++ tests/test_unit/data/test_crud.py
@@ -0,0 +1,200 @@
+from collections.abc import Generator
+from time import sleep
+
+import pytest
+from sqlalchemy.exc import NoResultFound
+from sqlalchemy.orm.exc import ObjectDeletedError
+from sqlmodel import Field, Session, SQLModel, inspect
+
+from logger.setup import get_logger
+from data.mysql import engine
+from data.crud import BaseCRUD
+from data.base_sql_models import BaseSQLModel
+
+
+class SQLModelForTest(BaseSQLModel, table=True):
+    """
+    The distinct sql model for test purposes
+    """
+    __tablename__ = "test"
+
+    title: str
+    year: int = Field(min_length=4, max_length=4)
+    description: str | None = Field(default=None)
+
+
+type CreatedTestEntry = Generator[BaseSQLModel, None, None]
+type CreatedTestEntries = Generator[list[SQLModelForTest | None], None, None]
+
+
+class SetUp:
+    test_data = [
+        {"title": "test_1", "year": 2025},
+        {"title": "test_2", "year": 2025},
+        {"title": "test_3", "year": 2025},
+        {"title": "test_4", "year": 2025},
+        {"title": "test_5", "year": 2025},
+    ]
+
+    def __init__(self, session: Session) -> None:
+        self.session = session
+
+    def create_entry(self, entry: BaseSQLModel) -> BaseSQLModel:
+        self.session.add(entry)
+        self.session.commit()
+        self.session.refresh(entry)
+        return entry
+
+    def tear_down(self, entry: BaseSQLModel) -> None:
+        try:
+            self._inspect(entry)
+        except ObjectDeletedError:
+            pass
+
+    def _inspect(self, entry: BaseSQLModel) -> None:
+        try:
+            state = inspect(entry)
+            if state.persistent:
+                self._delete_entry(entry)
+        except ObjectDeletedError:
+            pass
+
+    def _delete_entry(self, entry: BaseSQLModel) -> None:
+        self.session.delete(entry)
+        self.session.commit()
+
+
+@pytest.fixture
+def setup(session: Session) -> SetUp:
+    return SetUp(session)
+
+
+@pytest.fixture
+def create_table() -> None:
+    SQLModel.metadata.create_all(engine, tables=[SQLModelForTest.__table__])
+
+
+@pytest.fixture
+def model_data() -> dict:
+    return {"title": "test", "year": 2025}
+
+
+@pytest.fixture
+def model_for_test(request) -> SQLModelForTest:
+    model_data = request.param
+    return SQLModelForTest(**model_data)
+
+
+@pytest.fixture
+def crud(session: Session) -> BaseCRUD:
+    return BaseCRUD(session, SQLModelForTest)
+
+
+@pytest.fixture
+def create_test_entry(
+        setup: SetUp,
+        model_for_test: SQLModelForTest) -> CreatedTestEntry:
+    test_entry = setup.create_entry(model_for_test)
+    yield test_entry
+    setup.tear_down(test_entry)
+
+
+@pytest.fixture
+def create_multiple_test_entries(
+        setup: SetUp) -> CreatedTestEntries:
+    entries = []
+    for model_data in setup.test_data:
+        entry = SQLModelForTest(**model_data)
+        entry = setup.create_entry(entry)
+        entries.append(entry)
+    yield entries
+    for entry in entries:
+        setup.tear_down(entry)
+
+
+@pytest.fixture
+def update_data() -> dict[str, str]:
+    return {
+        "title": "updated",
+        "description": "title has been updated"
+    }
+
+
+@pytest.mark.usefixtures("create_table")
+class TestBaseCRUD:
+    @pytest.mark.parametrize(
+        "model_for_test", [SetUp.test_data[1]], indirect=True
+    )
+    def test_add_generates_default_values(
+            self,
+            crud: BaseCRUD,
+            model_for_test: SQLModelForTest) -> None:
+        entry = crud.add(model_for_test)
+        assert entry.title == SetUp.test_data[1].get("title")  # FIX: Remove this later
+        assert entry.id
+        assert entry.created_at
+        assert entry.updated_at
+
+    @pytest.mark.parametrize(
+        "model_for_test", SetUp.test_data, indirect=True
+    )
+    def test_get_returns_entry(
+            self,
+            crud: BaseCRUD,
+            create_test_entry: CreatedTestEntry) -> None:
+        entry = crud.get(create_test_entry.id)
+        assert entry.title == create_test_entry.title
+
+    def test_get_unexisting_entry(self, crud: BaseCRUD) -> None:
+        with pytest.raises(NoResultFound):
+            crud.get(0)
+    
+    def test_get_all_entries(
+            self,
+            create_multiple_test_entries: CreatedTestEntries,
+            crud: BaseCRUD) -> None:
+        entries = crud.get_all()
+        for entry_from_db, created_entry in zip(
+                entries, create_multiple_test_entries
+        ):
+            assert entry_from_db.title == created_entry.title
+
+    def test_get_all_from_empty_db(self, crud: BaseCRUD) -> None:
+        entries = crud.get_all()
+        assert entries == []
+
+    @pytest.mark.parametrize(
+        "model_for_test", [SetUp.test_data[0]], indirect=True
+    )
+    def test_update_succeed(
+            self,
+            create_test_entry: CreatedTestEntry,
+            crud: BaseCRUD,
+            update_data: dict[str, str]) -> None:
+        crud.update(create_test_entry, update_data)
+        assert create_test_entry.title == update_data.get("title")
+        assert create_test_entry.description == update_data.get("description")
+
+    @pytest.mark.parametrize(
+        "model_for_test", [SetUp.test_data[0]], indirect=True
+    )
+    def test_updated_time_is_updated(
+            self,
+            create_test_entry: CreatedTestEntry,
+            crud: BaseCRUD,
+            update_data: dict[str, str]) -> None:
+        old_update_at = create_test_entry.updated_at
+        sleep(1)
+        crud.update(create_test_entry, update_data)
+        assert old_update_at < create_test_entry.updated_at
+
+    @pytest.mark.parametrize(
+        "model_for_test", [SetUp.test_data[0]], indirect=True
+    )
+    def test_delete(
+            self,
+            create_test_entry: CreatedTestEntry,
+            crud: BaseCRUD) -> None:
+        crud.delete(create_test_entry)
+        with pytest.raises(NoResultFound):
+            crud.get(create_test_entry.id)
